c37da94093f458ce7349c7455cd89beb7ae9371c
Pieter Bergwerff <p.bergwerff@studiostaat.nl>
2020-03-31 02:33:10 +0200

    Changed ‘packages’ to ‘’services

--- core/index.js
+++ core/index.js
@@ -1,15 +1,15 @@
 const { resolve } = require("path");
 const BASE_PATH = resolve(__dirname, "..");
-const PACKAGES_PATH = resolve(BASE_PATH, "packages");
-const API_PATH = resolve(PACKAGES_PATH, "api");
-const FRONTEND_PATH = resolve(PACKAGES_PATH, "frontend");
-const FILESERVER_PATH = resolve(PACKAGES_PATH, "fileserver");
+const SERVICES_PATH = resolve(BASE_PATH, "services");
+const API_PATH = resolve(SERVICES_PATH, "api");
+const FRONTEND_PATH = resolve(SERVICES_PATH, "frontend");
+const FILESERVER_PATH = resolve(SERVICES_PATH, "fileserver");
 
 module.exports = {
   name: "Admin",
   version: "1.0",
   BASE_PATH,
-  PACKAGES_PATH,
+  SERVICES_PATH,
   API_PATH,
   FRONTEND_PATH,
   FILESERVER_PATH
